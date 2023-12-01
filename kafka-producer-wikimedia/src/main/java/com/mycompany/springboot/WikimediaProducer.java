a2ce7cfb36c9d637b4bda13ee661a6acda2f8e46
Vijay Saddi <vijaysaddi@Vijays-MacBook-Pro.local>
2023-11-30 21:13:36 -0500

    configured producer send data to kafka

---
+++ kafka-producer-wikimedia/src/main/java/com/mycompany/springboot/WikimediaProducer.java
@@ -0,0 +1,39 @@
+package com.mycompany.springboot;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class WikimediaProducer {
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaProducer.class);
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public WikimediaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage() throws InterruptedException {
+        String topic = "wikimedia_event";
+
+        EventHandler eventHandler = new WikimediaHandler(kafkaTemplate, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+        eventSource.start();
+
+        TimeUnit.MINUTES.sleep(10);
+    }
+}
